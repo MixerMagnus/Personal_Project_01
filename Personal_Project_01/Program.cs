ef74c21912aafc95156b68bfd2c5474f460bdf55
Kyler <kyler.kershas@gmail.com>
2018-05-12 14:18:42 -0400

    Step 1 Done
    Asked used if wanted to play.
    Defined rules of the game.

--- Personal_Project_01/Program.cs
+++ Personal_Project_01/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Threading;
 
-namespace Personal_Project_01
+
+// Goals of this project
+// 1. Ask the user if they want to play
+//     dev a system for yes or no
+// 2. Have user guess the number
+//     if too low or too high, clarify and start again
+// 3. if correct number is chosen, state so and end
+
+namespace Number_guessing_game
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+		Console.WriteLine("Would you like to play a game? (Y/N)");
+		string input = Console.ReadLine();
+			if ( input.ToLower() == "y")
+			{
+				//introdicing game rules
+            Console.WriteLine("I'm thinking of a number between 1 and 9...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Care to have a guess?");
+			}
+		else
+			{
+			Thread.Sleep(1000);
+			Console.WriteLine("I understand...Goodbye...");
+			}
+            
+
+
         }
     }
 }
