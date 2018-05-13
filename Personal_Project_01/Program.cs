71451c13e8677af97c5a7103cf2670a71f062a02
Kyler <kyler.kershas@gmail.com>
2018-05-12 20:00:25 -0400

    Restructured code
    Function list at bottom
    main game loop above

--- Personal_Project_01/Program.cs
+++ Personal_Project_01/Program.cs
@@ -3,68 +3,87 @@ using System.Threading;
 
 
 // Goals of this project
-// 1. Ask the user if they want to play (done)
-//     dev a system for yes or no (Done)
+// 1. Ask the user if they want to play ()
+//     dev a system for yes or no ()
 // 2. Have user guess the number ()
 //     if too low or too high, clarify and start again ()
 // 3. if correct number is chosen, state so and end ()
 
 namespace Number_guessing_game
 {
-    class MainClass
-    {
+	class MainClass
+	{
 
 		public static void Main(string[] args)
-        {
+		{
+            ///THIS IS THE GAME
+			asktoplay();
+			string input = Console.ReadLine();
+            if (input.ToLower() == "y")
+            {
+				Thread.Sleep(100);
+				Console.WriteLine("Let's begin..");
+            }
 
-		Console.WriteLine("Would you like to play a game? (Y/N)");
-		string input = Console.ReadLine();
-            //Assigning a random number for the game
-            //(Needed out of the main function to avoid the number changing every wrong answer.
-			Random NumMaker = new Random();
-			int randomNumber = NumMaker.Next(8) + 1;
-			if (input.ToLower() == "y")
+
+			Thread.Sleep(500);
+
+			startGame();
+
+			gameLoop();
+
+            ///End of game section
+		}
+       
+
+
+        //Create a function list
+        
+        //Function 1, Ask to play a game
+		static void asktoplay()
+		{
+			Console.WriteLine("Hello there...");
+			Thread.Sleep(1000);
+			Console.WriteLine("Would you like to press a game? (Y/N)");
+
+		}
+
+		//Function 2, Introduce Game rules
+        static void startGame()
+		{
+			Console.WriteLine("I'm thinking of a number between 1 and 9...");
+			Thread.Sleep(1500);
+			Console.WriteLine("Take a guess. Type your answer.");
+
+		}
+
+        static void gameLoop()
+		{
+			Random Nummaker = new Random();
+            int rannum = Nummaker.Next(8) + 1;
+			int uguess = 0;
+			uguess = Convert.ToInt32(Console.ReadLine());
+			while (uguess != rannum)
 			{
-				//Calling the function for the game
-				StartGame();
-
-				///Next line tests the RNG to make sure it works.
-				///Console.WriteLine(randomNumber);
-				int uinput = 0;
-				uinput = Convert.ToInt32(Console.ReadLine());
-				while (uinput != randomNumber)
+				if (uguess < rannum)
 				{
-					if (uinput < randomNumber)
-					{
-						Console.WriteLine("That's a bit too low.. Try again!");
-						return StartGame;
-					}
-					if (uinput > randomNumber)
-					{
-						Console.WriteLine("That's too high...Give it another shot!");
-						return StartGame;
-					}
+					Console.WriteLine("That's a bit too low...Try again..");
+					return;
 				}
-				if (uinput == randomNumber)
+
+                if (uguess > rannum)
 				{
-					Console.WriteLine("That's right! The number was ", randomNumber);
+					Console.WriteLine("That's too high. Give it another go!");
+					return;
+				}
+
+				if (uguess == rannum)
+				{
+					Console.WriteLine("That's right! The number was {0} ", rannum);
+					return;
 				}
 			}
-			else
-			{
-				Thread.Sleep(1000);
-                Console.WriteLine("I understand...Goodbye...");
-			}
-          
-
-		    }
-		static void StartGame()
-        {
-            //introdicing game rules
-            Console.WriteLine("I'm thinking of a number between 1 and 9...");
-            Thread.Sleep(1500);
-            Console.WriteLine("Take a guess and type your number.");
-        }
-        }
-
+		}
 	}
+
+}
