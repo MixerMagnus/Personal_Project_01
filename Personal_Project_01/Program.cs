eb86747da8f0cc77ec9b7240116d74786cfab38b
Kyler <kyler.kershas@gmail.com>
2018-05-13 16:39:38 -0400

    Variation on reply
    Created a switch system that sends variation of replies to the user based on random numbers.

--- Personal_Project_01/Program.cs
+++ Personal_Project_01/Program.cs
@@ -5,9 +5,9 @@ using System.Threading;
 // Goals of this project
 // 1. Ask the user if they want to play ()
 //     dev a system for yes or no ()
-// 2. Have user guess the number ()
-//     if too low or too high, clarify and start again ()
-// 3. if correct number is chosen, state so and end ()
+// 2. Have user guess the number (Done)
+//     if too low or too high, clarify and start again (Done)
+// 3. if correct number is chosen, state so and end (Done)
 
 namespace Number_guessing_game
 {
@@ -18,12 +18,16 @@ namespace Number_guessing_game
 		{
             //Calling needed parts
 			Random random = new Random();
-			int randomNumber = random.Next(1, 11);
+			int randomNumber = random.Next(1, 11);         
 			int guess;
 			int numberOfGuesses = 0;
 			bool gameover = false;
 
-            //Game rules listed
+			//Asking player if they want to play.
+			Console.WriteLine("Would you like to play a game? (Y|N)");
+
+
+			//Game rules listed
 			Console.WriteLine("I'm thinking of a number between 1 and 10...");
 			Thread.Sleep(500);
 			Console.WriteLine("Type your guess");
@@ -33,8 +37,9 @@ namespace Number_guessing_game
 			{
 				guess = Convert.ToInt32(Console.ReadLine());
 				numberOfGuesses++;
+				int randomREP = random.Next(4);
 
-				//User is correct or cheats
+				//User is correct
 				if (guess == randomNumber)
 				{
 					Thread.Sleep(500);
@@ -42,6 +47,7 @@ namespace Number_guessing_game
 					gameover = true;
 				}
 
+                //EASTER EGG
 				else if (guess == 1337)
                 {
                     Thread.Sleep(1000);
@@ -59,19 +65,69 @@ namespace Number_guessing_game
 				else if (guess > randomNumber)
 				{
 					Thread.Sleep(500);
-					Console.WriteLine("That's too high. Give it another go.");
+					switch(randomREP)
+					{
+						case 0:
+							{
+								Console.WriteLine("Too High my dude");
+								break;
+							}
+						case 1:
+                            {
+                                Console.WriteLine("A bit up there");
+                                break;
+                            }
+						case 2:
+                            {
+                                Console.WriteLine("Not quite. Try lower");
+                                break;
+                            }
+						case 3:
+                            {
+								Console.WriteLine("Shooting for the stars I see.");
+                                Thread.Sleep(5000);
+                                Console.WriteLine("Try again");
+                                break;
+                            }
+					}
 				}
 
                 //Guess is too low
 				else if (guess < randomNumber)
                 {
 					Thread.Sleep(500);
-					Console.WriteLine("That's too low my dude. Try again.");
+					switch(randomREP)
+					{
+						case 0:
+							{
+								Console.WriteLine("Too low");
+								break;
+							}
+						case 1:
+                            {
+								Console.WriteLine("Not quite...Try higher");
+                                break;
+
+                            }
+						case 2:
+                            {
+								Console.WriteLine("A bit too low");
+                                break;
+
+                            }
+						case 3:
+                            {
+								Console.WriteLine("Find anything digging that low?");                        
+                                break;
+
+                            }
+					}
                 }
 
 
 			}
 
+			Thread.Sleep(1000);
 			Console.WriteLine("G A M E  O V E R");
 			Console.ReadLine();
 
